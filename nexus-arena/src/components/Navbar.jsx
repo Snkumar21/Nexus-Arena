d0746449a9fc0a413ae22186ec27b4b57971aa6e
Nitish <snkumar6926@gmail.com>
2026-03-05 02:38:38 +0530

    New changes added in navbar

--- nexus-arena/src/components/Navbar.jsx
+++ nexus-arena/src/components/Navbar.jsx
@@ -1,14 +1,22 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/logo_icon.png";
 import "../App.css";
 
 function Navbar() {
     return (
         <nav className="navbar">
-            <div className="logo">Nexus Arena</div>
+            <div className="logo-container">
+                <img src={logo} alt="Nexus Arena" className="logo-img" />
+                <h2>Nexus Arena</h2>
+            </div>
+
             <ul className="nav-links">
-                <li>About</li>
-                <li>Services</li>
-                <li>Login</li>
-                <li className="signup-btn">Sign Up</li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/services">Services</Link></li>
+                <li><Link to="/login">Login</Link></li>
+                <li className="signup-btn">
+                    <Link to="/signup">Sign Up</Link>
+                </li>
             </ul>
         </nav>
     );
