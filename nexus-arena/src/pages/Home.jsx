4b7a00f8455f6239572eb6198fc606d1b138147f
Nitish <snkumar6926@gmail.com>
2026-03-05 12:37:41 +0530

    Background Updated

--- nexus-arena/src/pages/Home.jsx
+++ nexus-arena/src/pages/Home.jsx
@@ -5,10 +5,19 @@ import Services from "../components/Services";
 import HowItWorks from "../components/HowItWorks";
 import Testimonials from "../components/Testimonials";
 import Footer from "../components/Footer";
+import bg from "../assets/background.png";
 
 function Home() {
     return (
-        <>
+        <div
+            style={{
+                backgroundImage: `url(${bg})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                minHeight: "100vh"
+            }}
+        >
             <Navbar />
             <Hero />
             <About />
@@ -16,7 +25,7 @@ function Home() {
             <HowItWorks />
             <Testimonials />
             <Footer />
-        </>
+        </div>
     );
 }
 
