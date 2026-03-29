00ec102a69dba4419fbbf75e72272567784a85c9
Nitish <snkumar6926@gmail.com>
2026-03-29 15:17:50 +0530

    New Changes Added

--- nexus-arena/src/components/Services.jsx
+++ nexus-arena/src/components/Services.jsx
@@ -1,14 +1,37 @@
 function Services() {
     return (
         <section className="section dark">
-            <h2>Our Services</h2>
-            <ul className="services-list">
-                <li>✔ Course Comparison from Multiple Platforms</li>
-                <li>✔ Best Rated Courses Based on Reviews</li>
-                <li>✔ Discounted Premium Courses</li>
-                <li>✔ Assignment & Activity Submission</li>
-                <li>✔ Dual Certification</li>
-            </ul>
+            <h2>
+                Why <span className="light_salmon">Nexus Arena</span> is Best for You ?
+            </h2>
+
+            <p className="services-headlines-p">
+                Our Services - <span className="light_salmon">Make Education Easier!</span>
+            </p>
+
+            <div className="services-grid">
+
+                <div className="service-card">
+                    <p>📑Course Comparison from Multiple Platforms</p>
+                </div>
+
+                <div className="service-card">
+                    <p>⭐ Best Rated Courses Based on Reviews</p>
+                </div>
+
+                <div className="service-card">
+                    <p>🔖 Discounted Premium Courses</p>
+                </div>
+
+                <div className="service-card">
+                    <p>💡 Assignment & Activity Submission</p>
+                </div>
+
+                <div className="service-card">
+                    <p>🎓 Dual Certification</p>
+                </div>
+
+            </div>
         </section>
     );
 }
